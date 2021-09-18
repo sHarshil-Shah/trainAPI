72ce286e1c5509aec3d1092db96215bf50a9536c
HA20171511 <ha20171511@wipro.com>
2021-09-18 15:57:16 +0530

    init

---
+++ demo/src/test/java/com/api/train/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.train;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
