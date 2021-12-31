2dba84ef8404c9842cf2709955fb142fc412645c
HA20171511 <ha20171511@wipro.com>
2021-12-31 10:59:55 +0530

    temp

--- demo/src/main/java/com/train/demo/AuthController.java
+++ demo/src/main/java/com/train/demo/AuthController.java
@@ -29,6 +29,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// comment for git
+
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/auth")
